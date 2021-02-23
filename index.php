1ae48fe328682fd40388c030f5691a65df28a42c
Diego <Petronacidiego@gmail.com>
2021-02-23 15:07:06 +0100

    sistemato vecchio esercizio

--- index.php
+++ index.php
@@ -9,8 +9,21 @@
     echo $argv[1];
     echo $argv[2];
 
-    echo 'ciao ' . $name1 . $lastname1;
+    echo 'Ciao ' . $name1 . $spazio . $lastname1;
 
+    $string = "ciao, sono Diego Petronaci";
+    $newName = str_replace("Diego", "***", $string);
+    $newSurname = str_replace("Petronaci", "***", $string);
+
+
+    $string1 = "ciao, sono Diego Petronaci";
+    $newName1 = str_replace($name1, "***", $string1);
+
+    $prova = $_GET["name"];
+    $newName2 = str_replace($prova, "***", $string1);
+
+    $provaArray = ["ciao", 30, "ciao", "hey", "hola"];
+    var_dump($provaArray);
 ?>
 
 <!DOCTYPE html>
@@ -22,5 +35,9 @@
     </head>
     <body>
         <h1> Benvenuto <?php echo $name . $spazio . $lastname . $spazio ?>, il tuo nome è lungo <?php echo strlen($name) ?> lettere  </h1>
+        <h1><?php echo $newName ?>, il mio nome è lungo <?php echo strlen($name) ?> lettere  </h1>
+        <h1><?php echo $newSurname . $spazio ?></h1>
+
+        <h2><?php echo $newName2; ?> </h2>
     </body>
 </html>
